003bb291905f89758f67ea70261eb8bc117389a3
田嶋浩明 <>
2022-11-28 19:01:46 +0900

    ターゲットを外す処理（未完）

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -6,7 +6,6 @@
       const elm = document.querySelector(object.hookName);
       const trigger = elm.querySelectorAll(object.tagName);
       const triggerLength = trigger.length;
-      console.log(trigger);
       let index = 0;
       while (index < triggerLength) {
         trigger[index].addEventListener('click', (e) => this.clickHandler(e));
@@ -17,37 +16,18 @@
       e.preventDefault();
       e.stopPropagation();
       const target = e.currentTarget; //クリックした要素
-      const content = target.nextElementSibling;
-      if (content.style.display === 'block') {
-        content.style.display = 'none';
+      if (target.classList.contains('is-active')) {
+        target.classList.remove('is-active')
       } else {
-        content.style.display = 'block';
+        target.classList.add('is-active')
+      }
+      target.onblur = function(){
+        this.classList.remove('is-active')
       }
     }
   }
 
-  // アコーディオンのリセット
-  class ResetAccordion {
-    constructor(object) {
-      document.addEventListener('click', (e) => {
-        const elm = document.querySelector(object.hookName);
-        const trigger = elm.querySelectorAll(object.tagName);
-        const triggerLength = trigger.length;
-        let index = 0;
-        while (index < triggerLength) {
-          const content = trigger[index].nextElementSibling;
-          content.style.display = 'none';
-          index++;
-        }
-      })
-    }
-  }
-
   const mainHeadAccordion = new Accordion({
-    hookName: "#js-accordion",
-    tagName: ".p-main__account__act__btn"
-  });
-  const mainHeadResetAccordion = new ResetAccordion({
     hookName: "#js-accordion",
     tagName: ".p-main__account__act__btn"
   });
@@ -56,10 +36,5 @@
     hookName: ".js-accordion",
     tagName: ".p-main__head__filter__btn"
   });
-  const filterResetAccordion = new ResetAccordion({
-    hookName: ".js-accordion",
-    tagName: ".p-main__head__filter__btn"
-  });
-
 
 })();
