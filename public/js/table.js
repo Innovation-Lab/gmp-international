b464d86b0df315ef9ffd6a371785c031f03310e8
田嶋浩明 <>
2023-01-25 18:51:44 +0900

    テーブルのスクリプト調整

--- public/js/table.js
+++ public/js/table.js
@@ -1,23 +1,16 @@
 (() => {
-  const table = document.querySelector('table');
-  const tTable = document.getElementsByClassName('p-table')[0];
-  let lMainHead = document.getElementsByClassName('l-main__head')[0];
-  let pTableFoot = document.getElementsByClassName('p-table__foot')[0];
-  let headHeight = 0
-  let tFoot = 0
-  let bodyPadding = 48;
-  if(headHeight) {
-    headHeight = lMainHead.offsetHeight;
-  }
-  if(pTableFoot) {
-    tFoot = pTableFoot.offsetHeight;
-  }
-  // thの数を取得
-  let thCount = document.querySelector('thead tr').childElementCount;
-  // tableにカラムのスタイルを指定
-  table.style.gridTemplateColumns = 'repeat('+thCount+', minmax(max-content, 1fr))';
-  let tTableHeight = 'calc(100vh - '+ (headHeight + bodyPadding + tFoot ) +'px)';//-16 →ネガティブマージン分
-  if(tTable){
-    tTable.style.maxHeight = tTableHeight;
-  }
+  const tables = document.querySelectorAll('.p-table');
+  const mainHead = document.querySelector('.l-main__head');
+  const tableFoot = document.querySelector('.p-table__foot');
+  const bodyPadding = 48;
+  mainHeadHeight = mainHead?.offsetHeight;
+  tableFootHeight = tableFoot?.offsetHeight;
+
+  tables.forEach(table => {
+    let cTable = table.querySelector('table');
+    let thCount = table.querySelector('thead tr').childElementCount;
+    let tableHeight = 'calc(100vh - '+ (mainHeadHeight + tableFootHeight + bodyPadding - 15 ) +'px)';//-16 →ネガティブマージン分
+    cTable.style.gridTemplateColumns = 'repeat('+thCount+', minmax(max-content, 1fr))';
+    table.style.maxHeight = tableHeight;
+  })
 })();
