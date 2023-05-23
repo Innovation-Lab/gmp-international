70ef82c9fe9b08f2e9821acb59aa21c061c7ba6a
takumimurakami <78392283+takumimurakami@users.noreply.github.com>
2023-05-23 21:23:13 +0900

    [fix]ログイン時のバリデーション修正

--- resources/views/web/components/modal/_modal-login.blade.php
+++ resources/views/web/components/modal/_modal-login.blade.php
@@ -11,10 +11,13 @@
         <div class="p-login__auth p-login__auth--modal">
           {!! Form::open(['method' => 'POST', 'route' => 'login']) !!}
             <input class="mailbox" type="email" name="email" placeholder="メールアドレス">
+            @error('email')
+              <p class="c-txt c-txt--err">{{ $message }}</p>
+            @enderror
             <input class="passbox" type="password" name="password" placeholder="パスワード">
-            @foreach ($errors->all() as $error)
-              <p class="c-txt c-txt--err">{{ $error }}</p>
-            @endforeach
+            @error('password')
+              <p class="c-txt c-txt--err">{{ $message }}</p>
+            @enderror
             <input class="login" type="submit" name="button" value="ログイン">
           {!! Form::close() !!}
           <a class="c-btn c-btn--text" href="{{route('password.request')}}">パスワードを忘れた方はこちら</a>
