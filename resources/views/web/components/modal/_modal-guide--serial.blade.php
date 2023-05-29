474d1b89d84362f58e452db620edbf897e045036
SL10 <h.koyama@soushin-lab.co.jp>
2023-05-29 17:52:51 +0900

    [add]製品ごとのシリアルナンバー記載箇所の追加

--- resources/views/web/components/modal/_modal-guide--serial.blade.php
+++ resources/views/web/components/modal/_modal-guide--serial.blade.php
@@ -2,28 +2,90 @@
   <div class="modal__overlay" tabindex="-1">
     <div class="modal__container modal__container--min" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
       <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+      {{--3輪ベビーカー・カート用--}}
       <div class="modal__box">
         <header class="modal__header modal__header--column">
           <h2 class="modal__title" id="modal-1-title">
             シリアルナンバーについて
           </h2>
           <p class="modal__description">
-            写真の<b>赤枠内</b>にシリアルナンバーがあります。
-            入力するナンバーは、<b>赤字の箇所</b>になります。（例の場合<b>GMP 101614</b>）
+            製品画像の<b>赤枠位置</b>に貼付されている銀色のシールに、
+            <b>「GMP」「GMP-D」「D」</b>から始まる数字が記載されていますので、
+            そちらを入力してください。<b>※Maxi-Cosi、Quinny等の当社取扱製品はシリアルナンバー入力不要です。</b>
           </p>
         </header>
-        <main class="modal__content" id="modal-1-content">
-          <div class="modal__content__body">
-            <img src="{{asset('img/web/sample/guide--serial.png')}}">
+        <main class="modal__content modal__content--serial" id="modal-1-content">
+          <div class="modal__content__head">
+            <h3 class="modal__title modal__title--sub" id="modal-1-title">
+              3輪ベビーカー・カート<br>
+              <div class="product">・AirBuggy COCOシリーズ<br>・AirBuggy PET DOMEシリーズ</div>
+            </h3>
+          <div>
+          <div class="modal__content__body modal__content__body--serial">
+            <img src="{{asset('img/web/sample/guide--serial--coco.png')}}">
+            <p class="modal__description">
+              進行方向を基準に<b>左後輪の縦フレーム、ドリンクホルダーの裏側付近に</b>シリアルナンバーシールが貼付しております。
+            </p>
           </div>
-          <!-- <div class="modal__content__foot">
-            <div class="p-btnWrap p-btnWrap--center">
-              <button class="c-btn c-btn--back" aria-label="Close modal" data-micromodal-close>戻る</button>
-              <a class="c-btn c-btn--accent">削除する</a>
-            </div>
-          </div> -->
         </main>
       </div>
+
+      <!-- {{--BOX型3輪ペットカート--}}
+      <div class="modal__box">
+        <header class="modal__header modal__header--column">
+          <h2 class="modal__title" id="modal-1-title">
+            シリアルナンバーについて
+          </h2>
+          <p class="modal__description">
+            製品画像の<b>赤枠位置</b>に貼付されている銀色のシールに、
+            <b>「GMP」「GMP-D」「D」</b>から始まる数字が記載されていますので、
+            そちらを入力してください。<b>※Maxi-Cosi、Quinny等の当社取扱製品はシリアルナンバー入力不要です。</b>
+          </p>
+        </header>
+        <main class="modal__content modal__content--serial" id="modal-1-content">
+          <div class="modal__content__head">
+            <h3 class="modal__title modal__title--sub" id="modal-1-title">
+              BOX型3輪ペットカート<br>
+              <div class="product">・AirBuggy PET CUBEシリーズ（TWINKLE・NEST）</div>
+              <div class="desc">※Air Buggy for Dog CARRIAGEは前輪ステップの裏側に貼付しております。</div>
+            </h3>
+          <div>
+          <div class="modal__content__body modal__content__body--serial">
+            <img src="{{asset('img/web/sample/guide--serial--cube.png')}}">
+            <p class="modal__description">
+              <b>コット内側の支柱フレーム（上部にある横向きのフレーム）</b>にシリアルナンバーシールを貼付しております。
+            </p>
+          </div>
+        </main>
+      </div> -->
+
+      <!-- {{--ショッピングカート・ペットカート--}}
+      <div class="modal__box">
+        <header class="modal__header modal__header--column">
+          <h2 class="modal__title" id="modal-1-title">
+            シリアルナンバーについて
+          </h2>
+          <p class="modal__description">
+            製品画像の<b>赤枠位置</b>に貼付されている銀色のシールに、
+            <b>「GMP」「GMP-D」「D」</b>から始まる数字が記載されていますので、
+            そちらを入力してください。<b>※Maxi-Cosi、Quinny等の当社取扱製品はシリアルナンバー入力不要です。</b>
+          </p>
+        </header>
+        <main class="modal__content modal__content--serial" id="modal-1-content">
+          <div class="modal__content__head">
+            <h3 class="modal__title modal__title--sub" id="modal-1-title">
+              ショッピングカート・ペットカート<br>
+              <div class="product">・gowalkerシリーズ（For shopping・For pet）</div>
+            </h3>
+          <div>
+          <div class="modal__content__body modal__content__body--serial">
+            <img src="{{asset('img/web/sample/guide--serial--gowalker.png')}}">
+            <p class="modal__description">
+              進行方向を基準に<b>左後輪の縦フレーム側の後脚フレーム付近に</b>シリアルナンバーシールが貼付しております。
+            </p>
+          </div>
+        </main>
+      </div> -->
     </div>
   </div>
 </div>
