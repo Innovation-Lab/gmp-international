685e044e1dc2e492b4f6cb859629179596914e3f
Harima <y.harima@soushin-lab.co.jp>
2023-05-31 20:27:37 +0900

    [fix]ユーザー管理詳細

--- resources/views/admin/users/detail/_sidebar.blade.php
+++ resources/views/admin/users/detail/_sidebar.blade.php
@@ -12,14 +12,14 @@
       <ul class="p-list">
         @foreach([
           '会員番号' => 'No.000000123456',
-          '名前（フリガナ）' => '山田　太郎（ヤマダ　タロウ）',
+          '名前<span>（フリガナ）</span>' => '山田　太郎<span>（ヤマダ　タロウ）</span>',
           '電話番号' => '080-1234-5678',
           'メールアドレス' => 'gmp@sample.com',
           '住所' => '
           〒 1530001<br>
           東京都千代田区紀尾井町1-1-1<br>
           紀尾井町ビル16F',
-          '個人情報の取り扱いについて' => '同意する',
+          '新着情報、お得情報' => '受け取る',
           'アカウント作成日時' => '2023/04/04　10:12',
         ] as $key => $val)
         <li class="p-list__item">
@@ -30,12 +30,12 @@
             {!! $val !!}
           </div>
           @endforeach
-          <div class="p-list__memo">
-            <p class="p-list__label">管理メモ</p>
-            <div class="p-list__memo__area"></div>
-          </div>
         </li>
       </ul>
+      <div class="p-list__memo">
+        <p class="p-list__label">管理メモ</p>
+        <div class="p-list__memo__area c-scroll"></div>
+      </div>
     </div>
   </div>
 </div>
--- resources/views/admin/users/detail/index.blade.php
+++ resources/views/admin/users/detail/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.pages._default')
 @section('title', 'ユーザー管理')
 @section('content')
-<div class="p-detail">
+<div class="p-detail p-detail--user noBorder">
   <div class="l-detail">
     <div class="l-detail__head">
       {{-- 詳細ヘッド --}}
@@ -24,73 +24,78 @@
                     <h3 class="p-detail__sidebar__box__head__title">
                       登録製品情報
                     </h3>
-                    <a href="" class="c-button__2">登録製品を追加する</a>
+                    <a href="" class="c-textButton__icon c-textButton--gray">
+                      <svg class="icon"><use href="#add"/></svg>
+                      登録製品を追加する
+                    </a>
                   </div>
                   <div class="p-detail__main__box__body">
-                    {{-- ---------- リスト ---------- --}}
-                    <ul class="p-product">
-                      @for($list = 0; $list < 10; $list++)
-                      <li class="p-product__item">
-                        <div class="p-product__item__left">
-                          <img src="{{asset('img/web/product/airbuggy_coco_premire_newflame_blossom_front.png')}}" width="64px" height="64px">
-                        </div>
-                        <div class="p-product__item__right">
-                          <div class="p-product__item__head">
-                            <div class="p-product__item__ttl">
-                              <div class="item">AIRBUGGY</div>
-                              <div class="item">COCO PREMIER FROM BIRTH</div>
-                            </div>
-                            <div class="p-product__item__other">
-                              <a href="" class="c-button__2">編集</a>
-                              <div class="item">BLOSSOM</div>
-                            </div>
+                    <div class="p-productScroll c-scroll">
+                      <ul class="p-product p-product--list">
+                        @for($list = 0; $list < 10; $list++)
+                        <li class="p-product__item">
+                          <div class="p-product__item__left">
+                            <img src="{{asset('img/web/product/airbuggy_coco_premire_newflame_blossom_front.png')}}" width="64px" height="64px">
                           </div>
-                          <ul class="p-product__item__body">
-                            <li class="p-product__item__body__list">
-                              <div class="p-product__label">
-                                購入日
+                          <div class="p-product__item__right">
+                            <div class="p-product__item__head">
+                              <div class="p-product__item__ttl">
+                                <div class="item">AIRBUGGY</div>
+                                <div class="item">COCO PREMIER FROM BIRTH</div>
                               </div>
-                              <div class="p-product__data">
-                                2023/04/04
+                              <div class="p-product__item__other">
+                                <a href="" class="c-textButton">編集</a>
+                                <div class="color">BLOSSOM</div>
                               </div>
-                            </li>
-                            <li class="p-product__item__body__list">
-                              <div class="p-product__label">
-                                シリアルNo.
-                              </div>
-                              <div class="p-product__data">
-                                GMP123456789
-                              </div>
-                            </li>
-                            <li class="p-product__item__body__list">
-                              <div class="p-product__label">
-                                保証期間
-                              </div>
-                              <div class="p-product__data">
-                                12ヶ月
-                              </div>
-                            </li>
-                            <li class="p-product__item__body__list">
-                              <div class="p-product__label">
-                                購入店舗
-                              </div>
-                              <div class="p-product__data">
-                                エアバギー代々木公園本店
-                              </div>
-                            </li>
-                            <li class="p-product__item__body__list">
-                              <div class="p-product__label">
-                                管理メモ
-                              </div>
-                              <div class="p-product__data">
-                                2024/04/04　タイヤ交換
-                              </div>
-                            </li>
-                          </ul>
-                        </div>
-                      </li>
-                      @endfor
-                    </ul>
+                            </div>
+                            <ul class="p-product__item__body">
+                              <li class="p-product__item__body__list">
+                                <div class="p-product__label">
+                                  シリアルNo.
+                                </div>
+                                <div class="p-product__data">
+                                  GMP123456789
+                                </div>
+                              </li>
+                              <li class="p-product__item__body__list auto">
+                                <div class="p-product__label">
+                                  購入日
+                                </div>
+                                <div class="p-product__data">
+                                  2023/04/04
+                                </div>
+                              </li>
+                              <li class="p-product__item__body__list">
+                                <div class="p-product__label">
+                                  保証期間
+                                </div>
+                                <div class="p-product__data">
+                                  12ヶ月
+                                </div>
+                              </li>
+                              <li class="p-product__item__body__list large">
+                                <div class="p-product__label">
+                                  購入店舗
+                                </div>
+                                <div class="p-product__data">
+                                  エアバギー代々木公園本店
+                                </div>
+                              </li>
+                              <li class="p-product__item__body__list large">
+                                <div class="p-product__label">
+                                  管理メモ
+                                </div>
+                                <div class="p-product__data">
+                                  2024/04/04　タイヤ交換
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                        </li>
+                        @endfor
+                      </ul>
+                      {{-- ---------- リスト ---------- --}}
+                    </div>
                   </div>
                 </div>
               </div>
