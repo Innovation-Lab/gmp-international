c87a7c41950d31ea302440d6875bef41cfd28006
Harima <y.harima@soushin-lab.co.jp>
2023-06-02 19:09:33 +0900

    [fix]管理者管理＞編集に削除ボタン追加

--- resources/views/admin/staffs/edit/_head.blade.php
+++ resources/views/admin/staffs/edit/_head.blade.php
@@ -3,6 +3,9 @@
     <div class="container">
       <div class="p-detail__head__inner">
         <a href="{{route('admin.staffs.index')}}" class="c-button__2">戻る</a>
+        <div class="p-detail__head__action">
+          <div class="c-button__2" data-micromodal-trigger="modal-alert">削除</div>
+        </div>
       </div>
     </div>
   </div>
