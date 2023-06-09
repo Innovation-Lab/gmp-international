fb86056b0dff863bba39a0ba580edc6a103fea6c
takumimurakami <78392283+takumimurakami@users.noreply.github.com>
2023-06-10 00:37:26 +0900

    [fix]微調整

--- resources/views/admin/masters/_tab.blade.php
+++ resources/views/admin/masters/_tab.blade.php
@@ -15,13 +15,13 @@
     <li class="p-tabList__item">
       <a href="{{route('admin.masters.store')}}" class="p-tabList__label @if(Route::currentRouteName() == 'admin.masters.store') is-active @endif">
         店舗マスタ
-        <span>{{ \App\Models\MColor::query()->count() }}</span>
+        <span>{{ \App\Models\MShop::query()->count() }}</span>
       </a>
     </li>
     <li class="p-tabList__item">
       <a href="{{route('admin.masters.color')}}" class="p-tabList__label @if(Route::currentRouteName() == 'admin.masters.color') is-active @endif">
         カラーマスタ
-        <span>{{ \App\Models\MShop::query()->count() }}</span>
+        <span>{{ \App\Models\MColor::query()->count() }}</span>
       </a>
     </li>
   </ul>
