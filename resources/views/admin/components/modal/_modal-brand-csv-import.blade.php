561ce5e5de24cde4b8a31e6facd1079b97889504
SL10 <h.koyama@soushin-lab.co.jp>
2023-06-16 12:55:08 +0900

    [fix]CSV入力モーダルレイアウト

--- resources/views/admin/components/modal/_modal-brand-csv-import.blade.php
+++ resources/views/admin/components/modal/_modal-brand-csv-import.blade.php
@@ -8,19 +8,20 @@
         <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
       </header>
       <main class="modal__content" id="modal-1-content">
-      </main>
-      {!! Form::open(['method' => 'post', 'route' => 'admin.csv.brand.import', 'files' => true, 'id' => 'brandImportForm']) !!}
-        <div class="c-input ">
+        {!! Form::open(['method' => 'post', 'route' => 'admin.csv.user.import', 'files' => true, 'id' => 'userImportForm']) !!}
+        <div class="modal__content__input">
           <label for="result_csv" class="">
-            {{ Form::file('csv_file', ['id' => 'brand_file', 'style' => 'width: auto;', 'onchange' => "$('#brand_fake_text_box').val($(this).prop('files')[0].name)"]) }}
-            <input type="text" id="result_csv" value="ファイル選択" onClick="$('#brand_file').click();" style="display:none;">
+            {{ Form::file('csv_file', ['id' => 'file', 'style' => 'width: auto;', 'onchange' => "$('#fake_text_box').val($(this).prop('files')[0].name)"]) }}
+            <input type="text" id="result_csv" value="ファイル選択" onClick="$('#file').click();" style="display:none;">
           </label>
-          <input type="text" id="brand_fake_text_box" value="ファイルを選択" size="35" readonly onClick="$('#brand_file').click();">
+          <input type="text" id="fake_text_box" value="ファイルを選択" size="35" readonly onClick="$('#file').click();">
         </div>
-      {!! Form::close() !!}
+        {!! Form::close() !!}
+        <div class="modal__content__text">※重複するIDはスキップされます。</div>
+      </main>
       <footer class="modal__footer">
         <button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">戻る</button>
-        <button type="submit" form="brandImportForm" class="modal__btn">実行する</button>
+        <button type="submit" form="brandImportForm" class="modal__btn-primary">実行する</button>
       </footer>
     </div>
   </div>
