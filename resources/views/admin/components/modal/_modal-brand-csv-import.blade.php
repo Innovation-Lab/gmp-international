566a3f517b3f96444d4aaaedbf9a5170802370f3
takumimurakami <78392283+takumimurakami@users.noreply.github.com>
2023-06-20 14:58:54 +0900

    [fix]

--- resources/views/admin/components/modal/_modal-brand-csv-import.blade.php
+++ resources/views/admin/components/modal/_modal-brand-csv-import.blade.php
@@ -8,13 +8,13 @@
         <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
       </header>
       <main class="modal__content" id="modal-1-content">
-        {!! Form::open(['method' => 'post', 'route' => 'admin.csv.user.import', 'files' => true, 'id' => 'userImportForm']) !!}
+        {!! Form::open(['method' => 'post', 'route' => 'admin.csv.brand.import', 'files' => true, 'id' => 'brandImportForm']) !!}
         <div class="modal__content__input">
           <label for="result_csv" class="">
-            {{ Form::file('csv_file', ['id' => 'file', 'style' => 'width: auto;', 'onchange' => "$('#fake_text_box').val($(this).prop('files')[0].name)"]) }}
-            <input type="text" id="result_csv" value="ファイル選択" onClick="$('#file').click();" style="display:none;">
+            {{ Form::file('csv_file', ['id' => 'brand_file', 'style' => 'width: auto;', 'onchange' => "$('#brand_fake_text_box').val($(this).prop('files')[0].name)"]) }}
+            <input type="text" id="result_csv" value="ファイル選択" onClick="$('#brand_file').click();" style="display:none;">
           </label>
-          <input type="text" id="fake_text_box" value="ファイルを選択" size="35" readonly onClick="$('#file').click();">
+          <input type="text" id="brand_fake_text_box" value="ファイルを選択" size="35" readonly onClick="$('#brand_file').click();">
         </div>
         {!! Form::close() !!}
         <div class="modal__content__text">※重複するIDはスキップされます。</div>
